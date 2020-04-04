71d8831234549215c02aa52524bf49198bae7e94
VCD <VeryCrazyDog@gmail.com>
2020-04-04 18:27:00 +0800

    Trim down test case data

--- src/test/data/school-mysqldump.sql
+++ src/test/data/school-mysqldump.sql
@@ -16,40 +16,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `courses`
---
-
-DROP TABLE IF EXISTS `courses`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `courses` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `code` varchar(10) NOT NULL,
-  `location` varchar(50) NOT NULL DEFAULT '',
-  `teacher_id` int(11) DEFAULT NULL,
-  `version` int(11) NOT NULL DEFAULT '1',
-  `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `modified_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `uk_code` (`code`),
-  KEY `ix_name` (`name`),
-  KEY `fk_courses_teachers_id` (`teacher_id`),
-  CONSTRAINT `fk_courses_teachers_id` FOREIGN KEY (`teacher_id`) REFERENCES `teachers` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `courses`
---
-
-LOCK TABLES `courses` WRITE;
-/*!40000 ALTER TABLE `courses` DISABLE KEYS */;
-INSERT INTO `courses` VALUES (1,'Chinese','course01','Lecture Theatre A',1,1,'2020-04-04 09:33:45','2020-04-04 09:33:45'),(2,'English','course02','Room 1',2,1,'2020-04-04 09:33:45','2020-04-04 09:33:45'),(3,'Maths','course03','Lecture Theatre B',2,1,'2020-04-04 09:33:45','2020-04-04 09:33:45'),(4,'Physics','course04','Room 2',3,1,'2020-04-04 09:33:45','2020-04-04 09:33:45'),(5,'Chemistry','course05','Lecture Theatre C',6,1,'2020-04-04 09:33:45','2020-04-04 09:33:45'),(6,'Biology','course06','Room 3',7,1,'2020-04-04 09:33:45','2020-04-04 09:33:45'),(7,'Geography','course07','Lecture Theatre D',7,1,'2020-04-04 09:33:45','2020-04-04 09:33:45'),(8,'Economics','course08','Room 2',7,1,'2020-04-04 09:33:45','2020-04-04 09:33:45'),(9,'Accounting','course09','Lecture Theatre E',8,1,'2020-04-04 09:33:45','2020-04-04 09:33:45'),(10,'Chinese History','course10','',NULL,1,'2020-04-04 09:33:45','2020-04-04 09:33:45');
-/*!40000 ALTER TABLE `courses` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `students`
 --
 
@@ -68,7 +34,7 @@ CREATE TABLE `students` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `uk_code` (`code`),
   KEY `ix_name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -77,7 +43,7 @@ CREATE TABLE `students` (
 
 LOCK TABLES `students` WRITE;
 /*!40000 ALTER TABLE `students` DISABLE KEYS */;
-INSERT INTO `students` VALUES (1,'Amy','student1','F','1980-01-01',1,'2020-04-04 09:33:45','2020-04-04 09:33:45'),(2,'Benny','student2','M','1981-02-02',1,'2020-04-04 09:33:45','2020-04-04 09:33:45'),(3,'Carmen','student3','F','1981-03-03',1,'2020-04-04 09:33:45','2020-04-04 09:33:45'),(4,'David','student4','M','1980-04-04',1,'2020-04-04 09:33:45','2020-04-04 09:33:45'),(5,'Edith','student5','F','1983-05-05',2,'2020-04-04 09:33:45','2020-04-04 09:33:45'),(6,'Fanny','student6','F','1980-06-06',2,'2020-04-04 09:33:45','2020-04-04 09:33:45'),(7,'Gabriel','student7','M','1981-07-07',2,'2020-04-04 09:33:45','2020-04-04 09:33:45'),(8,'Henry','student8','M','1980-08-08',2,'2020-04-04 09:33:45','2020-04-04 09:33:45'),(9,'Ivan','student9','M','1982-09-09',3,'2020-04-04 09:33:45','2020-04-04 09:33:45'),(10,'Janice','student10','F','1983-10-10',3,'2020-04-04 09:33:45','2020-04-04 09:33:45'),(11,'Keith','student11','M','1983-11-11',4,'2020-04-04 09:33:45','2020-04-04 09:33:45'),(12,'Ling','student12','F','1982-12-12',5,'2020-04-04 09:33:45','2020-04-04 09:33:46'),(13,'April','student13','F','1980-01-13',2,'2020-04-04 09:33:45','2020-04-04 09:33:45');
+INSERT INTO `students` VALUES (1,'Amy','student1','F','1980-01-01',1,'2020-04-04 10:25:28','2020-04-04 10:25:28'),(2,'Benny','student2','M','1981-02-02',1,'2020-04-04 10:25:28','2020-04-04 10:25:28'),(3,'Carmen','student3','F','1981-03-03',1,'2020-04-04 10:25:28','2020-04-04 10:25:28'),(4,'David','student4','M','1980-04-04',2,'2020-04-04 10:25:28','2020-04-04 10:25:28'),(5,'Edith','student5','F','1983-05-05',1,'2020-04-04 10:25:28','2020-04-04 10:25:28'),(6,'Fanny','student6','F','1980-06-06',1,'2020-04-04 10:25:28','2020-04-04 10:25:28'),(7,'Gabriel','student7','M','1981-07-07',1,'2020-04-04 10:25:28','2020-04-04 10:25:28'),(8,'Henry','student8','M','1980-08-08',1,'2020-04-04 10:25:28','2020-04-04 10:25:28'),(9,'Ivan','student9','M','1982-09-09',1,'2020-04-04 10:25:28','2020-04-04 10:25:28'),(10,'Janice','student10','F','1983-10-10',1,'2020-04-04 10:25:28','2020-04-04 10:25:28');
 /*!40000 ALTER TABLE `students` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
@@ -160,36 +126,6 @@ DELIMITER ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
--- Table structure for table `students_courses`
---
-
-DROP TABLE IF EXISTS `students_courses`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `students_courses` (
-  `student_id` int(11) NOT NULL,
-  `course_id` int(11) NOT NULL,
-  `registered_date` date NOT NULL,
-  `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `modified_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`student_id`,`course_id`),
-  UNIQUE KEY `uk_students_courses` (`course_id`,`student_id`),
-  CONSTRAINT `fk_students_courses_courses_id` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`),
-  CONSTRAINT `fk_students_courses_students_id` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `students_courses`
---
-
-LOCK TABLES `students_courses` WRITE;
-/*!40000 ALTER TABLE `students_courses` DISABLE KEYS */;
-INSERT INTO `students_courses` VALUES (1,1,'2005-09-01','2020-04-04 09:33:45','2020-04-04 09:33:45'),(1,2,'2005-09-01','2020-04-04 09:33:45','2020-04-04 09:33:45'),(1,3,'2005-09-01','2020-04-04 09:33:45','2020-04-04 09:33:45'),(2,2,'2005-09-01','2020-04-04 09:33:45','2020-04-04 09:33:45'),(2,4,'2005-09-15','2020-04-04 09:33:45','2020-04-04 09:33:45'),(5,1,'2005-09-01','2020-04-04 09:33:45','2020-04-04 09:33:45'),(5,4,'2005-09-01','2020-04-04 09:33:45','2020-04-04 09:33:45'),(5,8,'2005-10-02','2020-04-04 09:33:45','2020-04-04 09:33:45'),(5,9,'2005-09-01','2020-04-04 09:33:45','2020-04-04 09:33:45'),(5,10,'2005-09-01','2020-04-04 09:33:45','2020-04-04 09:33:45'),(8,3,'2005-09-13','2020-04-04 09:33:45','2020-04-04 09:33:45'),(9,3,'2005-09-01','2020-04-04 09:33:45','2020-04-04 09:33:45'),(9,8,'2005-09-01','2020-04-04 09:33:45','2020-04-04 09:33:45'),(12,3,'2005-09-01','2020-04-04 09:33:45','2020-04-04 09:33:45'),(12,8,'2005-09-01','2020-04-04 09:33:45','2020-04-04 09:33:45');
-/*!40000 ALTER TABLE `students_courses` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `students_history`
 --
 
@@ -210,7 +146,7 @@ CREATE TABLE `students_history` (
   `modified_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `uk_student_id_version` (`student_id`,`version`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -219,39 +155,8 @@ CREATE TABLE `students_history` (
 
 LOCK TABLES `students_history` WRITE;
 /*!40000 ALTER TABLE `students_history` DISABLE KEYS */;
-INSERT INTO `students_history` VALUES (1,1,1,'A','2020-04-04 09:33:45','Amy','student1','F','1980-01-01','2020-04-04 09:33:45','2020-04-04 09:33:45'),(2,2,1,'A','2020-04-04 09:33:45','Benny','student2','M','1981-02-02','2020-04-04 09:33:45','2020-04-04 09:33:45'),(3,3,1,'A','2020-04-04 09:33:45','Carmen','student3','F','1981-03-03','2020-04-04 09:33:45','2020-04-04 09:33:45'),(4,4,1,'A','2020-04-04 09:33:45','David','student4','M','1980-04-04','2020-04-04 09:33:45','2020-04-04 09:33:45'),(5,5,2,'A','2020-04-04 09:33:45','Edith','student5','F','1983-05-05','2020-04-04 09:33:45','2020-04-04 09:33:45'),(6,6,2,'A','2020-04-04 09:33:45','Fanny','student6','F','1980-06-06','2020-04-04 09:33:45','2020-04-04 09:33:45'),(7,7,2,'A','2020-04-04 09:33:45','Gabriel','student7','M','1981-07-07','2020-04-04 09:33:45','2020-04-04 09:33:45'),(8,8,2,'A','2020-04-04 09:33:45','Henry','student8','M','1980-08-08','2020-04-04 09:33:45','2020-04-04 09:33:45'),(9,9,3,'A','2020-04-04 09:33:45','Ivan','student9','M','1982-09-09','2020-04-04 09:33:45','2020-04-04 09:33:45'),(10,10,3,'A','2020-04-04 09:33:45','Janice','student10','F','1983-10-10','2020-04-04 09:33:45','2020-04-04 09:33:45'),(11,11,4,'A','2020-04-04 09:33:45','Keith','student11','M','1983-11-11','2020-04-04 09:33:45','2020-04-04 09:33:45'),(12,12,4,'A','2020-04-04 09:33:45','Ling','student12',NULL,'1982-12-12','2020-04-04 09:33:45','2020-04-04 09:33:45'),(13,13,2,'A','2020-04-04 09:33:45','April','student13','F','1980-01-13','2020-04-04 09:33:45','2020-04-04 09:33:45'),(14,12,5,'M','2020-04-04 09:33:46','Ling','student12','F','1982-12-12','2020-04-04 09:33:46','2020-04-04 09:33:46');
+INSERT INTO `students_history` VALUES (1,1,1,'A','2020-04-04 10:25:28','Amy','student1','F','1980-01-01','2020-04-04 10:25:28','2020-04-04 10:25:28'),(2,2,1,'A','2020-04-04 10:25:28','Benny','student2','M','1981-02-02','2020-04-04 10:25:28','2020-04-04 10:25:28'),(3,3,1,'A','2020-04-04 10:25:28','Carmen','student3','F','1981-03-03','2020-04-04 10:25:28','2020-04-04 10:25:28'),(4,4,1,'A','2020-04-04 10:25:28','David','student4',NULL,'1980-04-04','2020-04-04 10:25:28','2020-04-04 10:25:28'),(5,5,1,'A','2020-04-04 10:25:28','Edith','student5','F','1983-05-05','2020-04-04 10:25:28','2020-04-04 10:25:28'),(6,6,1,'A','2020-04-04 10:25:28','Fanny','student6','F','1980-06-06','2020-04-04 10:25:28','2020-04-04 10:25:28'),(7,7,1,'A','2020-04-04 10:25:28','Gabriel','student7','M','1981-07-07','2020-04-04 10:25:28','2020-04-04 10:25:28'),(8,8,1,'A','2020-04-04 10:25:28','Henry','student8','M','1980-08-08','2020-04-04 10:25:28','2020-04-04 10:25:28'),(9,9,1,'A','2020-04-04 10:25:28','Ivan','student9','M','1982-09-09','2020-04-04 10:25:28','2020-04-04 10:25:28'),(10,10,1,'A','2020-04-04 10:25:28','Janice','student10','F','1983-10-10','2020-04-04 10:25:28','2020-04-04 10:25:28'),(11,4,2,'M','2020-04-04 10:25:28','David','student4','M','1980-04-04','2020-04-04 10:25:28','2020-04-04 10:25:28');
 /*!40000 ALTER TABLE `students_history` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `teachers`
---
-
-DROP TABLE IF EXISTS `teachers`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `teachers` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `code` varchar(10) NOT NULL,
-  `gender` char(1) DEFAULT NULL,
-  `version` int(11) NOT NULL DEFAULT '1',
-  `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `modified_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `uk_code` (`code`),
-  KEY `ix_name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `teachers`
---
-
-LOCK TABLES `teachers` WRITE;
-/*!40000 ALTER TABLE `teachers` DISABLE KEYS */;
-INSERT INTO `teachers` VALUES (1,'Andrew','teacher1','M',1,'2020-04-04 09:33:45','2020-04-04 09:33:45'),(2,'Boris','teacher2','M',1,'2020-04-04 09:33:45','2020-04-04 09:33:45'),(3,'Carol','teacher3','F',1,'2020-04-04 09:33:45','2020-04-04 09:33:45'),(4,'Doris','teacher4',NULL,1,'2020-04-04 09:33:45','2020-04-04 09:33:45'),(5,'Edison','teacher5','M',1,'2020-04-04 09:33:45','2020-04-04 09:33:45'),(6,'Fiona','teacher6','F',1,'2020-04-04 09:33:45','2020-04-04 09:33:45'),(7,'Grace','teacher7','F',1,'2020-04-04 09:33:45','2020-04-04 09:33:45'),(8,'Helen','teacher8','F',1,'2020-04-04 09:33:45','2020-04-04 09:33:45'),(9,'Issac','teacher9','M',1,'2020-04-04 09:33:45','2020-04-04 09:33:45'),(10,'Jack','teacher10','M',1,'2020-04-04 09:33:45','2020-04-04 09:33:45');
-/*!40000 ALTER TABLE `teachers` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -271,4 +176,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-04-04 17:34:01
+-- Dump completed on 2020-04-04 18:26:22
--- src/test/data/school.sql
+++ src/test/data/school.sql
@@ -22,44 +22,6 @@ CREATE TABLE students (
 	UNIQUE uk_code (code)
 );
 
-CREATE TABLE teachers (
-	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(50) NOT NULL,
-	code VARCHAR(10) NOT NULL,
-	gender CHAR,
-	version INT NOT NULL DEFAULT 1,
-	created_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	modified_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	INDEX ix_name (name),
-	UNIQUE uk_code (code)
-);
-
-CREATE TABLE courses (
-	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(50) NOT NULL,
-	code VARCHAR(10) NOT NULL,
-	location VARCHAR(50) NOT NULL DEFAULT '',
-	teacher_id INT,
-	version INT NOT NULL DEFAULT 1,
-	created_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	modified_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	INDEX ix_name (name),
-	UNIQUE uk_code (code),
-	CONSTRAINT fk_courses_teachers_id FOREIGN KEY (teacher_id) REFERENCES teachers(id)
-);
-
-CREATE TABLE students_courses (
-	student_id INT NOT NULL,
-	course_id INT NOT NULL,
-	registered_date DATE NOT NULL,
-	created_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	modified_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	PRIMARY KEY (student_id, course_id),
-	CONSTRAINT uk_students_courses UNIQUE KEY (course_id, student_id),
-	CONSTRAINT fk_students_courses_students_id FOREIGN KEY (student_id) REFERENCES students(id),
-	CONSTRAINT fk_students_courses_courses_id FOREIGN KEY (course_id) REFERENCES courses(id)
-);
-
 CREATE TABLE students_history (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	student_id INT NOT NULL,
@@ -124,70 +86,21 @@ DELIMITER ;
 -- -----------------------------------------------------------------------------
 -- Insert initial data
 -- -----------------------------------------------------------------------------
-INSERT students (id, name, code, gender, birthday, version) VALUES
-	(1, 'Amy', 'student1', 'F', '1980-01-01', 1),
-	(2, 'Benny', 'student2', 'M', '1981-02-02', 1),
-	(3, 'Carmen', 'student3', 'F', '1981-03-03', 1),
-	(4, 'David', 'student4', 'M', '1980-04-04', 1),
-	(5, 'Edith', 'student5', 'F', '1983-05-05', 2),
-	(6, 'Fanny', 'student6', 'F', '1980-06-06', 2),
-	(7, 'Gabriel', 'student7', 'M', '1981-07-07', 2),
-	(8, 'Henry', 'student8', 'M', '1980-08-08', 2),
-	(9, 'Ivan', 'student9', 'M', '1982-09-09', 3),
-	(10, 'Janice', 'student10', 'F', '1983-10-10', 3),
-	(11, 'Keith', 'student11', 'M', '1983-11-11', 4),
-	(12, 'Ling', 'student12', NULL, '1982-12-12', 4),
-	(13, 'April', 'student13', 'F', '1980-01-13', 2);
-
-INSERT teachers (id, name, code, gender) VALUES
-	(1, 'Andrew', 'teacher1', 'M'),
-	(2, 'Boris', 'teacher2', 'M'),
-	(3, 'Carol', 'teacher3', 'F'),
-	(4, 'Doris', 'teacher4', NULL),
-	(5, 'Edison', 'teacher5', 'M'),
-	(6, 'Fiona', 'teacher6', 'F'),
-	(7, 'Grace', 'teacher7', 'F'),
-	(8, 'Helen', 'teacher8', 'F'),
-	(9, 'Issac', 'teacher9', 'M'),
-	(10, 'Jack', 'teacher10', 'M');
-
-INSERT courses (id, name, code, location, teacher_id) VALUES
-	(1, 'Chinese', 'course01', 'Lecture Theatre A', 1),
-	(2, 'English', 'course02', 'Room 1', 2),
-	(3, 'Maths', 'course03', 'Lecture Theatre B', 2),
-	(4, 'Physics', 'course04', 'Room 2', 3),
-	(5, 'Chemistry', 'course05', 'Lecture Theatre C', 6),
-	(6, 'Biology', 'course06', 'Room 3', 7),
-	(7, 'Geography', 'course07', 'Lecture Theatre D', 7),
-	(8, 'Economics', 'course08', 'Room 2', 7),
-	(9, 'Accounting', 'course09', 'Lecture Theatre E', 8),
-	(10, 'Chinese History', 'course10', '', NULL);
-
-INSERT students_courses(student_id, course_id, registered_date) VALUES
-	(1, 1, '2005-09-01'),
-	(1, 2, '2005-09-01'),
-	(1, 3, '2005-09-01'),
-	(2, 2, '2005-09-01'),
-	(2, 4, '2005-09-15'),
-	(5, 1, '2005-09-01'),
-	(5, 4, '2005-09-01'),
-	(5, 8, '2005-10-02'),
-	(5, 9, '2005-09-01'),
-	(5, 10, '2005-09-01'),
-	(8, 3, '2005-09-13'),
-	(9, 3, '2005-09-01'),
-	(9, 8, '2005-09-01'),
-	(12, 3, '2005-09-01'),
-	(12, 8, '2005-09-01');
+INSERT students (id, name, code, gender, birthday) VALUES
+	(1, 'Amy', 'student1', 'F', '1980-01-01'),
+	(2, 'Benny', 'student2', 'M', '1981-02-02'),
+	(3, 'Carmen', 'student3', 'F', '1981-03-03'),
+	(4, 'David', 'student4', NULL, '1980-04-04'),
+	(5, 'Edith', 'student5', 'F', '1983-05-05'),
+	(6, 'Fanny', 'student6', 'F', '1980-06-06'),
+	(7, 'Gabriel', 'student7', 'M', '1981-07-07'),
+	(8, 'Henry', 'student8', 'M', '1980-08-08'),
+	(9, 'Ivan', 'student9', 'M', '1982-09-09'),
+	(10, 'Janice', 'student10', 'F', '1983-10-10');
 
 -- -----------------------------------------------------------------------------
--- Update some data and check history
+-- Update data to trigger trigger
 -- -----------------------------------------------------------------------------
-SELECT SLEEP(1);
-UPDATE students
-	SET
-		gender = 'F',
-		version = version + 1
-	WHERE name = 'Ling';
-
-SELECT * FROM students_history;
+UPDATE (SELECT SLEEP(1)) dummy, students
+	SET gender = 'M', version = version + 1
+	WHERE name = 'David';
