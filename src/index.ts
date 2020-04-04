9cd68b7ce13cd6f1c66cbbb6a3f9ce7b357d423d
VCD <VeryCrazyDog@gmail.com>
2020-04-04 19:10:58 +0800

    Fix lint errors

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,37 @@ export interface SplitOptions {
   multipleStatements?: boolean
 }
 
+// Not able to split long URL
+// eslint-disable-next-line max-len
 // https://github.com/Bajdzis/vscode-database/blob/1cbe33bd63330d08c931fc8ef46d199f0c8ae597/src/extension/engine/mysql-pass.ts#L156
-function removeComments (sql: string) {
+function removeComments (sql: string): string {
+  // eslint-disable-next-line max-len
   const quotes = /^((?:[^"`']*?(?:(?:"(?:[^"]|\\")*?(?<!\\)")|(?:'(?:[^']|\\')*?(?<!\\)')|(?:`(?:[^`]|\\`)*?(?<!\\)`)))*?[^"`']*?)/
   const cStyleComments = new RegExp(quotes.source + '/\\*.*?\\*/')
   const doubleDashComments = new RegExp(quotes.source + '--(?:(?:[ \t]+.*(\r\n|\n|\r)?)|(\r\n|\n|\r)|$)')
   const hashComments = new RegExp(quotes.source + '#.*(\r\n|\n|\r)?')
-  while (sql.match(cStyleComments)) sql = sql.replace(cStyleComments, '$1')
-  while (sql.match(doubleDashComments)) sql = sql.replace(doubleDashComments, '$1$2$3')
-  while (sql.match(hashComments)) sql = sql.replace(hashComments, '$1$2')
+  while (sql.match(cStyleComments) !== null) {
+    sql = sql.replace(cStyleComments, '$1')
+  }
+  while (sql.match(doubleDashComments) !== null) {
+    sql = sql.replace(doubleDashComments, '$1$2$3')
+  }
+  while (sql.match(hashComments) !== null) {
+    sql = sql.replace(hashComments, '$1$2')
+  }
   return sql
 }
 
+// Not able to split long URL
+// eslint-disable-next-line max-len
 // https://github.com/Bajdzis/vscode-database/blob/1cbe33bd63330d08c931fc8ef46d199f0c8ae597/src/extension/engine/mysql-pass.ts#L122
-function splitQueries (sqlMulti: string) {
+function splitQueries (sqlMulti: string): string[] {
+  // eslint-disable-next-line max-len
   const quotes = /^((?:[^"`']*?(?:(?:"(?:[^"]|\\")*?(?<!\\)")|(?:'(?:[^']|\\')*?(?<!\\)')|(?:`(?:[^`]|\\`)*?(?<!\\)`)))*?[^"`']*?)/
   const delimiterRegex = /^(?:\r\n|[ \t\r\n])*DELIMITER[\t ]*(.*?)(?:\r\n|\n|\r|$)/i
-  const queries = []; let match: any = []; let delimiter = ';'
+  const queries = []
+  let match: RegExpMatchArray | null = []
+  let delimiter = ';'
   let splitRegex = new RegExp(quotes.source + delimiter)
   while (match !== null) {
     const delimiterCommand = sqlMulti.match(delimiterRegex)
@@ -37,7 +51,7 @@ function splitQueries (sqlMulti: string) {
   queries.push(sqlMulti) // push last query which could have no delimiter
   // remove empty queries
   return queries.filter((sql) => {
-    if (!sql) {
+    if (sql === '') {
       return false
     }
     const notEmpty = (sql.trim().replace(/(\r\n|\n|\r)/gm, '') !== '')
@@ -47,6 +61,8 @@ function splitQueries (sqlMulti: string) {
 
 export function split (sql: string, options?: SplitOptions): string[] {
   options = options ?? {}
+  // TODO Implement options
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const multipleStatements = options.multipleStatements ?? false
 
   return splitQueries(removeComments(sql))
