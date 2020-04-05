64f80c4b7843494fb7a87f79c6b78f4b5c35f88a
VCD <VeryCrazyDog@gmail.com>
2020-04-05 21:41:50 +0800

    Add more test cases

--- src/test/split-mysqldump.test.ts
+++ src/test/split-mysqldump.test.ts
@@ -58,15 +58,15 @@ test('should have correct 10th statement', t => {
   t.is(statements[9], '/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */')
 })
 
-test('should have correct 11th statement', t => {
+test('should have correct 11st statement', t => {
   t.is(statements[10], 'DROP TABLE IF EXISTS `students`')
 })
 
-test('should have correct 12th statement', t => {
+test('should have correct 12nd statement', t => {
   t.is(statements[11], '/*!40101 SET @saved_cs_client     = @@character_set_client */')
 })
 
-test('should have correct 13th statement', t => {
+test('should have correct 13rd statement', t => {
   t.is(statements[12], '/*!40101 SET character_set_client = utf8 */')
 })
 
@@ -119,15 +119,15 @@ test('should have correct 20th statement', t => {
   t.is(statements[19], 'UNLOCK TABLES')
 })
 
-test('should have correct 21th statement', t => {
+test('should have correct 21st statement', t => {
   t.is(statements[20], '/*!50003 SET @saved_cs_client      = @@character_set_client */')
 })
 
-test('should have correct 22th statement', t => {
+test('should have correct 22nd statement', t => {
   t.is(statements[21], '/*!50003 SET @saved_cs_results     = @@character_set_results */')
 })
 
-test('should have correct 23th statement', t => {
+test('should have correct 23rd statement', t => {
   t.is(statements[22], '/*!50003 SET @saved_col_connection = @@collation_connection */')
 })
 
@@ -180,102 +180,113 @@ test('should have correct 29th statement', t => {
   ].join('\r\n'))
 })
 
-// test('should have correct xth statement', t => {
-//   t.is(statements[x], "LOCK TABLES `students_history` WRITE;")
-// })
+test('should have correct 30th statement', t => {
+  t.is(statements[29], '/*!50003 SET sql_mode              = @saved_sql_mode */')
+})
 
-// test('should have correct xth statement', t => {
-//   t.is(statements[x], "/*!40000 ALTER TABLE `students_history` DISABLE KEYS */;")
-// })
+test('should have correct 31st statement', t => {
+  t.is(statements[30], '/*!50003 SET character_set_client  = @saved_cs_client */')
+})
 
-// test('should have correct xth statement', t => {
-//   t.is(statements[x], "INSERT INTO `students_history` VALUES (1,1,1,'A','2020-04-04 10:25:28','Amy','student1','F','1980-01-01','2020-04-04 10:25:28','2020-04-04 10:25:28'),(2,2,1,'A','2020-04-04 10:25:28','Benny','student2','M','1981-02-02','2020-04-04 10:25:28','2020-04-04 10:25:28'),(3,3,1,'A','2020-04-04 10:25:28','Carmen','student3','F','1981-03-03','2020-04-04 10:25:28','2020-04-04 10:25:28'),(4,4,1,'A','2020-04-04 10:25:28','David','student4',NULL,'1980-04-04','2020-04-04 10:25:28','2020-04-04 10:25:28'),(5,5,1,'A','2020-04-04 10:25:28','Edith','student5','F','1983-05-05','2020-04-04 10:25:28','2020-04-04 10:25:28'),(6,6,1,'A','2020-04-04 10:25:28','Fanny','student6','F','1980-06-06','2020-04-04 10:25:28','2020-04-04 10:25:28'),(7,7,1,'A','2020-04-04 10:25:28','Gabriel','student7','M','1981-07-07','2020-04-04 10:25:28','2020-04-04 10:25:28'),(8,8,1,'A','2020-04-04 10:25:28','Henry','student8','M','1980-08-08','2020-04-04 10:25:28','2020-04-04 10:25:28'),(9,9,1,'A','2020-04-04 10:25:28','Ivan','student9','M','1982-09-09','2020-04-04 10:25:28','2020-04-04 10:25:28'),(10,10,1,'A','2020-04-04 10:25:28','Janice','student10','F','1983-10-10','2020-04-04 10:25:28','2020-04-04 10:25:28'),(11,4,2,'M','2020-04-04 10:25:28','David','student4','M','1980-04-04','2020-04-04 10:25:28','2020-04-04 10:25:28');")
-// })
+test('should have correct 32nd statement', t => {
+  t.is(statements[31], '/*!50003 SET character_set_results = @saved_cs_results */')
+})
 
-// test('should have correct xth statement', t => {
-//   t.is(statements[x], "/*!40000 ALTER TABLE `students_history` ENABLE KEYS */;")
-// })
+test('should have correct 33rd statement', t => {
+  t.is(statements[32], '/*!50003 SET collation_connection  = @saved_col_connection */')
+})
 
-// test('should have correct xth statement', t => {
-//   t.is(statements[x], "UNLOCK TABLES;")
-// })
+test('should have correct 34th statement', t => {
+  t.is(statements[33], 'DROP TABLE IF EXISTS `students_history`')
+})
 
-// test('should have correct xth statement', t => {
-//   t.is(statements[x], "")
-// })
+test('should have correct 35th statement', t => {
+  t.is(statements[34], '/*!40101 SET @saved_cs_client     = @@character_set_client */')
+})
 
-// test('should have correct xth statement', t => {
-//   t.is(statements[x], "--")
-// })
+test('should have correct 36th statement', t => {
+  t.is(statements[35], '/*!40101 SET character_set_client = utf8 */')
+})
 
-// test('should have correct xth statement', t => {
-//   t.is(statements[x], "-- Dumping events for database 'school'")
-// })
+test('should have correct 37th statement', t => {
+  t.is(statements[36], [
+    'CREATE TABLE `students_history` (',
+    '  `id` int(11) NOT NULL AUTO_INCREMENT,',
+    '  `student_id` int(11) NOT NULL,',
+    '  `version` int(11) NOT NULL,',
+    '  `action` char(1) NOT NULL,',
+    '  `action_time` timestamp NOT NULL,',
+    '  `name` varchar(50) NOT NULL,',
+    '  `code` varchar(10) NOT NULL,',
+    '  `gender` char(1) DEFAULT NULL,',
+    '  `birthday` date DEFAULT NULL,',
+    '  `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,',
+    '  `modified_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,',
+    '  PRIMARY KEY (`id`),',
+    '  UNIQUE KEY `uk_student_id_version` (`student_id`,`version`)',
+    ') ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4'
+  ].join('\r\n'))
+})
 
-// test('should have correct xth statement', t => {
-//   t.is(statements[x], "--")
-// })
+test('should have correct 38th statement', t => {
+  t.is(statements[37], '/*!40101 SET character_set_client = @saved_cs_client */')
+})
 
-// test('should have correct xth statement', t => {
-//   t.is(statements[x], "")
-// })
+test('should have correct 39th statement', t => {
+  t.is(statements[38], 'LOCK TABLES `students_history` WRITE')
+})
 
-// test('should have correct xth statement', t => {
-//   t.is(statements[x], "--")
-// })
+test('should have correct 40th statement', t => {
+  t.is(statements[39], '/*!40000 ALTER TABLE `students_history` DISABLE KEYS */')
+})
 
-// test('should have correct xth statement', t => {
-//   t.is(statements[x], "-- Dumping routines for database 'school'")
-// })
+test('should have correct 41st statement', t => {
+  t.is(statements[40], [
+    'INSERT INTO `students_history` VALUES ',
+    "(1,4,2,'M','2020-04-05 13:25:26','David','student4','M','1980-04-04','2020-04-05 13:25:26','2020-04-05 13:25:26')"
+  ].join(''))
+})
 
-// test('should have correct xth statement', t => {
-//   t.is(statements[x], "--")
-// })
+test('should have correct 42nd statement', t => {
+  t.is(statements[41], '/*!40000 ALTER TABLE `students_history` ENABLE KEYS */')
+})
 
-// test('should have correct xth statement', t => {
-//   t.is(statements[x], "/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;")
-// })
+test('should have correct 43rd statement', t => {
+  t.is(statements[42], 'UNLOCK TABLES')
+})
 
-// test('should have correct xth statement', t => {
-//   t.is(statements[x], "")
-// })
+test('should have correct 44th statement', t => {
+  t.is(statements[43], '/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */')
+})
 
-// test('should have correct xth statement', t => {
-//   t.is(statements[x], "/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;")
-// })
+test('should have correct 45th statement', t => {
+  t.is(statements[44], '/*!40101 SET SQL_MODE=@OLD_SQL_MODE */')
+})
 
-// test('should have correct xth statement', t => {
-//   t.is(statements[x], "/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;")
-// })
+test('should have correct 46th statement', t => {
+  t.is(statements[45], '/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */')
+})
 
-// test('should have correct xth statement', t => {
-//   t.is(statements[x], "/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;")
-// })
+test('should have correct 47th statement', t => {
+  t.is(statements[46], '/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */')
+})
 
-// test('should have correct xth statement', t => {
-//   t.is(statements[x], "/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;")
-// })
+test('should have correct 48th statement', t => {
+  t.is(statements[47], '/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */')
+})
 
-// test('should have correct xth statement', t => {
-//   t.is(statements[x], "/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;")
-// })
+test('should have correct 49th statement', t => {
+  t.is(statements[48], '/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */')
+})
 
-// test('should have correct xth statement', t => {
-//   t.is(statements[x], "/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;")
-// })
+test('should have correct 50th statement', t => {
+  t.is(statements[49], '/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */')
+})
 
-// test('should have correct xth statement', t => {
-//   t.is(statements[x], "/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;")
-// })
+test('should have correct 51st statement', t => {
+  t.is(statements[50], '/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */')
+})
 
-// test('should have correct xth statement', t => {
-//   t.is(statements[x], "")
-// })
-
-// test('should have correct xth statement', t => {
-//   t.is(statements[x], "-- Dump completed on 2020-04-04 18:26:22")
-// })
-
-// test('should have correct xth statement', t => {
-//   t.is(statements[x], "")
-// })
+test('should have correct number of statements', t => {
+  t.is(statements.length, 51)
+})
