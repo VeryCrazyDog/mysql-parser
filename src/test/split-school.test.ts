b78a28997cbeeda6290d9d847fc450e4f2d3f5c8
VCD <VeryCrazyDog@gmail.com>
2020-04-05 20:42:01 +0800

    Add more test cases

--- src/test/split-school.test.ts
+++ src/test/split-school.test.ts
@@ -82,3 +82,65 @@ test('should have correct 6th statement', t => {
     'END'
   ].join('\r\n'))
 })
+
+test('should have correct 7th statement', t => {
+  t.is(statements[6], [
+    'CREATE  TRIGGER students_after_update AFTER UPDATE ON students FOR EACH ROW',
+    'BEGIN',
+    '  INSERT INTO students_history SET',
+    '    student_id = NEW.id,',
+    '    version = NEW.version,',
+    "    action = 'M',",
+    '    action_time = NEW.modified_time,',
+    '    name = NEW.name,',
+    '    code = NEW.code,',
+    '    gender = NEW.gender,',
+    '    birthday = NEW.birthday;',
+    'END'
+  ].join('\r\n'))
+})
+
+test('should have correct 8th statement', t => {
+  t.is(statements[7], [
+    'CREATE  TRIGGER students_after_delete AFTER DELETE ON students FOR EACH ROW',
+    'BEGIN',
+    '  INSERT INTO students_history SET',
+    '    student_id = OLD.id,',
+    '    version = OLD.version + 1,',
+    "    action = 'D',",
+    '    action_time = CURRENT_TIMESTAMP,',
+    '    name = OLD.name,',
+    '    code = OLD.code,',
+    '    gender = OLD.gender,',
+    '    birthday = OLD.birthday;',
+    'END'
+  ].join('\r\n'))
+})
+
+test('should have correct 9th statement', t => {
+  t.is(statements[8], [
+    'INSERT students (id, name, code, gender, birthday) VALUES',
+    "  (1, 'Amy', 'student1', 'F', '1980-01-01'),",
+    "  (2, 'Benny', 'student2', 'M', '1981-02-02'),",
+    "  (3, 'Carmen', 'student3', 'F', '1981-03-03'),",
+    "  (4, 'David', 'student4', NULL, '1980-04-04'),",
+    "  (5, 'Edith', 'student5', 'F', '1983-05-05'),",
+    "  (6, 'Fanny', 'student6', 'F', '1980-06-06'),",
+    "  (7, 'Gabriel', 'student7', 'M', '1981-07-07'),",
+    "  (8, 'Henry', 'student8', 'M', '1980-08-08'),",
+    "  (9, 'Ivan', 'student9', 'M', '1982-09-09'),",
+    "  (10, 'Janice', 'student10', 'F', '1983-10-10')"
+  ].join('\r\n'))
+})
+
+test('should have correct 10th statement', t => {
+  t.is(statements[9], [
+    'UPDATE (SELECT SLEEP(1)) dummy, students',
+    "  SET gender = 'M', version = version + 1",
+    "  WHERE name = 'David'"
+  ].join('\r\n'))
+})
+
+test('should have correct number of statements', t => {
+  t.is(statements.length, 10)
+})
